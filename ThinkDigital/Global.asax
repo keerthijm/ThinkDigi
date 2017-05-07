d520ad5930f08aa61255babeed00e29d1eff7d8b
Keerthi Jagalur Mutt <keerthijm@gmail.com>
2017-05-07 16:23:23 +0100

    Add project files.

---
+++ ThinkDigital/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ThinkDigital.MvcApplication" Language="C#" %>
---
+++ ThinkDigital/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThinkDigital.Startup))]
+namespace ThinkDigital
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
